313f9090f3c5a72b278538fd440c7998b1300ab1
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-17 10:03:17 -0800

    Toggled Darkmode

--- client/src/App.js
+++ client/src/App.js
@@ -27,14 +27,14 @@ render () {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Womens Worl Cup</h1>
+        {/* <h1>Womens Worl Cup</h1>
         <p>name: "Alex Morgan", country: "United States", searches: 100, id: 0</p>
         <p>Complete Players List Components</p>
         <p>Import Players List</p>
         <p>Complete Players Card</p>
         <p>Import Players Card to PlayersList</p>
         <p>Complete LocalStorage, Dardmode, NavBar and use DarkMode NavBar</p>
-        <p>Import NavBar</p>
+        <p>Import NavBar</p> */}
         <Navbar/>
         <PlayersList players={this.state.players}/>
       </header>
--- client/src/components/PlayersCard.js
+++ client/src/components/PlayersCard.js
@@ -4,7 +4,7 @@ import { Card, CardSubtitle } from "reactstrap";
 const PlayersCard = props => {
   return (
     <Card className="players-card">
-      <CardSubtitle>Testing Players Card</CardSubtitle>
+      {/* <CardSubtitle>Testing Players Card</CardSubtitle> */}
       <CardSubtitle>Name: {props.player.name}</CardSubtitle>
       <CardSubtitle>Country: {props.player.country}</CardSubtitle>
       <CardSubtitle>ID: {props.player.id}</CardSubtitle>
