6e0c8f6fcb13abdf57f2c9adc6fe2338d1977271
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-17 08:45:56 -0800

    completed axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    players: []
+
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      this.setState({
+        players: res.data
+      })
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+  }
+
+render () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Womens Worl Cup</h1>
+        <p>name: "Alex Morgan", country: "United States", searches: 100, id: 0</p>
+        <p>Complete Players List Components</p>
+        <p>Import Players List</p>
+        <p>Complete Players Card</p>
+        <p>Import Players Card to PlayersList</p>
       </header>
     </div>
   );
+
+}
+  
 }
 
 export default App;
