1d7a329edaf2f89204f6d9c9d6df1fc9e7ccf23c
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-17 08:53:36 -0800

    PlayersList.js Completed

---
+++ client/src/components/PlayersCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+
+  const PlayersCard = props => {
+    return (
+       <Card>
+           <CardSubtitle>Testing Players Card</CardSubtitle>
+       </Card>
+
+    )
+}
+
+export default PlayersCard;
--- client/src/components/PlayersList.js
+++ client/src/components/PlayersList.js
@@ -1,7 +1,18 @@
 import react from 'react'
-
+// import PlayersCard from './PlayersCard';
 
 const PlayersList = props => {
 
-    return 
+    return (
+
+    <div>
+        <div className="playerslist">
+          <PlayersCard user={this.state.players} />
+          {props.players.map((player) => (
+            <PlayersCard key={player.id} player={player}/>
+          ))}
+        </div>
+    </div>
+
+    )
 }
